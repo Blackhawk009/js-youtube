9d941860bf47fcfd144ebe9a2558c04a347aa4a1
Blackhawk009 <aarbinstha644@gmail.com>
2025-01-01 07:47:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail="aarbinstha@gmail.com";
+var accountPassword="12345";
+accountCity="Kathmandu";
+let accountState;
+
+
+// accountId=2
+accountEmail="ar@gmail.com"
+accountPassword="1122"
+accountCity="banepa"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+// alert("hello")
+
+console.log("my name");
+console.log(3 + 3);
+
+let name="arbin";
+let age=19;
+let isLoggedIn=true;
+
+/*Data Types
+int => number
+bigint 
+string => ""
+boolean => true or false
+null => stand alone value( let x = null)
+undefined => not defined( let x;)
+symbol => unique
+*/
+
+/*object
+
+
+*/
+
