fe7686b2608d8900ca976f1a4d75c1dafa53d5c9
Blackhawk009 <aarbinstha644@gmail.com>
2025-01-01 08:23:59 +0000

    updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,19 +1,35 @@
-let score=false;
-console.log(typeof score);
+// let score=false;
+// console.log(typeof score);
 
-let valueInNumber=Number(score)
-console.log(typeof (valueInNumber));
-console.log(valueInNumber); 
-
-
-
-let isLoggedIn=2;
-let BooleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(typeof BooleanIsLoggedIn);
-console.log(BooleanIsLoggedIn);
+// let valueInNumber=Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber); 
 
 
-let someNumber=12;
-let stringNumber=String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+
+// let isLoggedIn=2;
+// let BooleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(typeof BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
+
+
+// let someNumber=12;
+// let stringNumber=String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+
+
+// ***************Operations*****************
+
+let value=3;
+let negvalue=-value;
+// console.log(negvalue);
+
+let str1="hello";
+let str2=" arbin";
+
+let str3=str1 + str2
+console.log(str3)
