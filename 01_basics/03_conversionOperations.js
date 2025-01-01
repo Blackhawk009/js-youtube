4db598c86d59c3ab3668ed6f869c9e0f1bda08f0
Blackhawk009 <aarbinstha644@gmail.com>
2025-01-01 08:06:03 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score=false;
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber); 
+
+
+
+let isLoggedIn=2;
+let BooleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof BooleanIsLoggedIn);
+console.log(BooleanIsLoggedIn);
+
+
+let someNumber=12;
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
